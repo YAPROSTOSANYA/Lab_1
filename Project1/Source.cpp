263440f6774bee468e8be836e464a987d25301a7
YAPROSTOSANYA <sashagrabun@mail.ru>
2025-02-18 00:23:27 +0300

    sec_com

---
+++ Project1/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m, a, s1, s2, N;
+	bool bn = true, bm = true;
+	cin >> n >> m >> a;
+	
+	//Проверяем, вместится ли плитка в нач. пол.
+	if ((n % a) > 0) bn = false;
+	if ((m % a) > 0) bm = false;
+
+	//Если не вместится, то ищем новые размеры поля
+	if (!(bn == true)) n += a - (n % a);
+	if (!(bm == true)) m += a - (m % a);
+
+	//Находим кол-во плиток
+	s1 = n * m;
+	s2 = a * a;
+	N = s1 / s2;
+	cout << N;
+
+	return 0;
+}
